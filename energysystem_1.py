437452552882b59d31172c2703ff506ab67f5df2
CarinaGeyer <carina.geyer@gmail.com>
2018-06-22 10:31:57 +0200

    3 capex scenarios and creating dataframe of each

--- energysystem_1.py
+++ energysystem_1.py
@@ -10,6 +10,8 @@ import os
 import pandas as pd
 import warnings
 
+
+
 solver = 'cbc'  # 'glpk', 'gurobi',....
 debug = False  # Set number_of_timesteps to 3 to get a readable lp-file.
 solver_verbose = False  # show/hide solver output
@@ -23,13 +25,49 @@ solver_verbose = False  # show/hide solver output
 #wind and pv feed in and electricity demand : 
 #fuelprices & development : Prognos AG 2013; Hecking et al. 2017; Schlesinger et al. 2014; World Bank 2017; DLR Rheinland-Pfalz 2017; Scheftelowitz et al. 2016
 #efficiency : Wietschel et al. 2010
-date_time_index = pd.date_range('1/1/2018', periods = 8760, freq = 'H' )
+
+date_time_index = pd.date_range('1/1/2018', periods = 10, freq = 'H' )
 
 energysystem = solph.EnergySystem(timeindex=date_time_index)
 
 #input_data
 filename = os.path.join(os.path.dirname(__file__), 'normalised data.csv')
 data = pd.read_csv(filename)
+
+
+
+
+###################TASK before running the optimization #######################
+##### set different boundary conditions for different scenarios by hand ######
+
+# do not change!
+sum_nominal_load_values = 6348.4153 # fixed value of the normalized el_demand series
+
+####################### choose el_demand scenario #############################
+# use the choosen variable for the actuel value in the network SINK
+nominal_BAU = 420000000/sum_nominal_load_values # business as usual 
+nominal_traffic_heat = 506000000/sum_nominal_load_values # traffic and heat included 
+
+
+############## select  Co2- certificates costs in [€/t]#######################
+CO2_cost = 16
+
+
+######################### choose capex scenarios  ############################
+# index[0] for conservative
+# index[1] for neutral
+# index[2] for optimistic
+
+filename1 = os.path.join(os.path.dirname(__file__), 'capex_scenarios_renewables.xlsx')
+capex_scenarios = pd.read_excel(filename1)
+
+capex_wind_on   = capex_scenarios.loc[capex_scenarios.index[2],'capex_wind_on']
+capex_wind_off  = capex_scenarios.loc[capex_scenarios.index[2], 'capex_wind_off']
+capex_pv        = capex_scenarios.loc[capex_scenarios.index[2], 'capex_pv']
+
+###################### END of secetion part ##################################
+
+
 
 # investment for each energy carrier
 epc_coal_old = economics.annuity(capex= (1650 + 32)*1000, n=40, wacc=0.056)
@@ -42,11 +80,11 @@ epc_lignite_new = economics.annuity(capex= (1900 + 36)*1000, n=40, wacc=0.056)
 
 epc_gas = economics.annuity(capex=(950 + 22)*1000, n=30, wacc=0.052)
 
-epc_wind_on = economics.annuity(capex=(1750 + 30)*1000, n=25, wacc=0.025)
+epc_wind_on = economics.annuity(capex=capex_wind_on, n=25, wacc=0.025)
 
-epc_wind_off = economics.annuity(capex=(3900 + 100)*1000, n=25, wacc=0.048)
+epc_wind_off = economics.annuity(capex=capex_wind_off, n=25, wacc=0.048)
 
-epc_pv = economics.annuity(capex=(1.025 *700)*1000, n=25, wacc=0.021)
+epc_pv = economics.annuity(capex=capex_pv, n=25, wacc=0.021)
 
 
 ###########
@@ -69,14 +107,13 @@ bel = solph.Bus(label = "electricity")
 # add all Buses to the EnergySystem
 energysystem.add( bcoal, blignite, bgas, bel)
 
-# Co2 Kosten €/t
-CO2_cost = 16
+
+
 
 # Cos costs €/MWh
-CO2_gas = 0.202 * CO2_cost 
-CO2_lignite = 0.337 * CO2_cost
-CO2_coal = 0.403 * CO2_cost
-
+CO2_gas     =    0.202 * CO2_cost 
+CO2_lignite =    0.337 * CO2_cost
+CO2_coal    =    0.403 * CO2_cost
 
 
 # source coal old
@@ -101,9 +138,9 @@ energysystem.add(solph.Source(label='rgas', outputs={bgas: solph.Flow(
 
 
 # EEG
-eeg_wind_on = -7
+eeg_wind_on  = -7
 eeg_wind_off = -1.9
-eeg_Pv = -22
+eeg_Pv       = -22
 # source wind onshore
 energysystem.add(solph.Source(label='wind_on', outputs={bel: solph.Flow(fixed=True, 
         actual_value=data['Wind_on'],variable_costs = eeg_wind_on , investment = solph.Investment(ep_costs=epc_wind_on,maximum=1187840))}))
@@ -117,11 +154,7 @@ energysystem.add(solph.Source(label='pv', outputs={bel: solph.Flow(fixed=True,
         actual_value=data['PV'], variable_costs = eeg_Pv, investment = solph.Investment(ep_costs=epc_pv,maximum=275000))}))
 
 
-# create simple sink object for electrical demand for each electrical bus
 
-sum_nominal_load_values = 6348.4153
-nominal_BAU = 420000000/sum_nominal_load_values
-nominal_traffic_heat = 506000000/sum_nominal_load_values
 
 solph.Sink(label='demand_elec', inputs={bel: solph.Flow(
        actual_value=data ['normalised_load_profile'] , fixed=True, nominal_value= nominal_BAU)})
--- optimization.py
+++ optimization.py
@@ -11,7 +11,7 @@ from oemof.outputlib import processing
 from oemof.outputlib import views
 
 import energysystem_1 as es
-
+import os
 
 import pandas as pd
 
@@ -21,44 +21,86 @@ import pprint as pp
 import matplotlib.pyplot as plt 
 
 
+filename1 = os.path.join(os.path.dirname(__file__), 'capex_scenarios_renewables.xlsx')
+capex_scenarios = pd.read_excel(filename1)
 #import networkx as nx
+################################
+# Optimizing of the energy system 
+################################
 
+    
 om = solph.Model(es.energysystem)
-
 om.solve(solver = 'cbc', solve_kwargs ={'tee':True})
-
 my_results = processing.results(om)
 
-
-
+###############################
+#accessing data of the optimization
+###############################
 
 es.energysystem.results['main'] = processing.results(om)
 es.energysystem.results['meta'] = processing.meta_results(om)
-
-# The default path is the '.oemof' folder in your $HOME directory.
-# The default filename is 'es_dump.oemof'.
-# You can omit the attributes (as None is the default value) for testing cases.
-# You should use unique names/folders for valuable results to avoid
-# overwriting.
-
-# store energy system with results
+#
 es.energysystem.dump(dpath=None, filename=None)
-# define an alias for shorter calls below (optional)
+    # define an alias for shorter calls below (optional)
 results = es.energysystem.results['main']
 
-# geeting data of the Data Frame
+######## create different Data Frame for the different scearnaios#############
+conservatice_df = pd.DataFrame()
+neutral_df = pd.DataFrame()
+optimistic_df = pd.DataFrame()
+
+if es.capex_wind_on == capex_scenarios.loc[capex_scenarios.index[0],'capex_wind_on']:
+        
+    conservative_df = processing.create_dataframe(om)
+    print('#### this is the conservative dataframe#########')
+    fn = os.path.join(os.path.dirname(__file__), 'conservative_DataFrame.xlsx')
+    pd.DataFrame(conservative_df).to_excel(fn)
+    
+if es.capex_wind_on == capex_scenarios.loc[capex_scenarios.index[1],'capex_wind_on']:
+    
+    neutral_df = processing.create_dataframe(om)
+    print('#### this is the neutral scenario#########')
+    fn = os.path.join(os.path.dirname(__file__), 'neutral_DataFrame.xlsx')
+    pd.DataFrame(conservative_df).to_excel(fn)
+    
+           
+if es.capex_wind_on == capex_scenarios.loc[capex_scenarios.index[2],'capex_wind_on']:
+    
+    optimistic_df = processing.create_dataframe(om)
+    print('#### this is the optimistic scenario#########')
+    fn = os.path.join(os.path.dirname(__file__), 'optimistic_DataFrame.xlsx')
+    pd.DataFrame(conservative_df).to_excel(fn)
+   
+    
+    
 df = processing.create_dataframe(om)
-print('--------------------DataFrame-----------------') 
-print(df)
-
-
-
 # creating a result dictionary containing node parameters
 p_results = processing.param_results(om)
-print('-------------------ParamResults---------------')
-print(p_results)
+
+################################
+#calculating material costs
+################################
+# showing the different capacities of each scenario
 
 
+#1 get the maximum capacities of each technology mbc = maximum built capacity
+mbc = df.loc[df.variable_name == 'invest', ['value', 'oemof_tuple']]
+
+# rename the index with the name of the investment flow 
+a = 0
+while a < mbc.index.size:
+    mbc.rename(index= { mbc.index[a]: (mbc.loc[mbc.index[a],'oemof_tuple'])}, inplace= True)
+    a+=1
+   
+print(mbc)
+
+# plot the max. installed capacity of all used technologies
+mbc.plot(kind = 'bar')
+
+# get max capacity of renewables
+max_capacity_pv       = mbc.loc[mbc.index[5], 'value']
+max_capacity_wind_off = mbc.loc[mbc.index[6], 'value']
+max_capacity_wind_on  = mbc.loc[mbc.index[7], 'value']
 
 
 electricity_bus     = views.node(results, 'electricity')
